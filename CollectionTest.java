33845153c0ed733e69e3dc81ffdd0f806e16a589
JinhyeongSeo <jh4clover@naver.com>
2024-04-16 14:09:37 +0900

    commit

---
+++ CollectionTest.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionTest {
+    int[] num = {4,2,3,1};
+    List<String> numList = new ArrayList<>();
+    numList
+
+    Collection.sort(num);
+    
+}
